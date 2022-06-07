de9afda4ee5d7c77aa934b2fb35a702cf6595659
Devrim Baris Acar <dbacar@stm.com.tr>
2022-06-07 17:50:30 +0300

    corrected poms for proper dependencies

--- spring-wso2-authorizer-xacml/src/main/java/com/example/demo/ConfigSecurity.java
+++ spring-wso2-authorizer-xacml/src/main/java/com/example/demo/ConfigSecurity.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
